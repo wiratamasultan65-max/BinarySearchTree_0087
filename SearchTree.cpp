1d4cf295fb8b02a6410cfd13add8f4069a9157d8
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-11 18:16:11 +0700

    Menambahkan case 1

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -132,3 +132,21 @@ int main()
         cout << "4.Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5): ";
+
+    char ch;
+        cin >> ch;
+        
+        cout << endl;
+
+        switch (ch)
+        {
+            case '1':
+            {
+                cout << "Enter a word: ";
+                string word;
+                cin >> word;
+                obj.insert(word);
+                break;
+            }
+
+           
