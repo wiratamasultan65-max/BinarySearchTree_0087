f6c73bcf2ffe57d414bd40a1b218360e55b06bd3
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-06-11 18:15:06 +0700

    menambahkan fungsi int main

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -118,3 +118,17 @@ void postorder(Node* ptr)
                 cout << ptr->info << " "; 
             }
         }    
+    };
+
+int main()
+{
+    BinaryTree obj;
+
+    while(true)
+    {
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4.Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
